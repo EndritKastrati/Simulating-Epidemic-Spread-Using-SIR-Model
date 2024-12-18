d6cabaddb0ae874b6c47bb400e0bd526d4e596dd
Endrit Kastrati <endritkastrati8@gmail.com>
2024-12-18 20:40:00 +0100

    Adding SIR model derivatives for infected and recovered populations

--- SIR/sir_model.py
+++ SIR/sir_model.py
@@ -9,3 +9,26 @@ def sir_derivative_s(S, I, beta):
         float: dS/dt.
     """
     return -beta * S * I
+
+def sir_derivative_i(S, I, gamma, beta):
+    """
+    Calculate dI/dt for the SIR model.
+    Parameters:
+        S (float): Susceptible population fraction.
+        I (float): Infected population fraction.
+        gamma (float): Recovery rate.
+        beta (float): Infection rate.
+    Returns:
+        float: dI/dt.
+    """
+    return beta * S * I - gamma * I
+def sir_derivative_r(I, gamma):
+    """
+    Calculate dR/dt for the SIR model.
+    Parameters:
+        I (float): Infected population fraction.
+        gamma (float): Recovery rate.
+    Returns:
+        float: dR/dt.
+    """
+    return gamma * I
